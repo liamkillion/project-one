72b14e8c9c620f7e11cb00ce3f2436ebd72c93e6
Liam Killion <liam.killion@gmail.com>
2017-10-23 16:16:01 -0400

    improved environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,19 @@
 require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-ActiveRecord::Base.logger = nil
-require_all 'app'
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+DBNAME = "arts.db"
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+
+DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+DB = ActiveRecord::Base.connection
+
+if ENV["ACTIVE_RECORD_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
