efd1ccd1da57c7fe98fdeaae968f2ce95f328c77
Fabiano Cunha <cunhasb@aol.com>
2017-10-26 14:28:10 -0400

    Add Rename, Create, Delete Collections

--- bin/run.rb
+++ bin/run.rb
@@ -2,8 +2,8 @@
 require_relative 'CLI.rb'
 
   Menu_1 = ["Personal Collections","Search Gallery","Exit"]
-  Menu_2 = ["Open Collection","Go Back"]
-  Menu_3 = ["Rename Collection","Delecte Collection","Show Pieces","Search for Pieces","Go Back"]
+  Menu_2 = ["Open Collection","Create Collection","Go Back"]
+  Menu_3 = ["Rename Collection","Delete Collection","Show Pieces","Search for Pieces","Go Back"]
  welcome
  user = get_user
  if user.has_collection == true
